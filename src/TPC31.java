24cd1a89f35ae58f2ec1a1a70fcaba04233ed925
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2022-09-02 19:18:29 +0900

    Java TPC (Private 생성자도 있어요? - static 과 관계)

---
+++ src/TPC31.java
@@ -0,0 +1,16 @@
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+    public static void main(String[] args) {
+
+        //Inflearn inf = new Inflearn();
+        //inf.tpc();
+        Inflearn.tpc();
+        //inf.java();
+        Inflearn.java();
+
+        // Java API 생성자 private
+
+
+    }
+}

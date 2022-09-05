851322d7a129296076f0aab74accfea65ffb339e
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2022-09-05 22:24:39 +0900

    Java TPC (애매하다! class, object, instance 상호관계)

--- src/kr/tpc/BookDTO.java
+++ src/kr/tpc/BookDTO.java
@@ -6,8 +6,14 @@ public class BookDTO {
     public String company;
     public int page;
     // 디폴트 생성자 메서드 (생략)
-//    public BookDTO () {
-//        //객체를 생성하는 작업한다. (기계어코드)
-//        super();
-//    }
+    public BookDTO () {
+        //객체를 생성하는 작업한다. (기계어코드)
+
+    }
+    public BookDTO(String title, int price, String company, int page) {
+        this.title = title;
+        this.price = price;
+        this.company = company;
+        this.page = page;
+    }
 }
